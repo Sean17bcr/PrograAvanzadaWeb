cc2315e330db1a445917ca35540234d5666195ff
Sean17bcr <106638574+Sean17bcr@users.noreply.github.com>
2023-09-28 11:04:44 -0600

    Primer Commit
    Se realizó la tarea y las plantillas que se van a usar

---
+++ ProyectoWeb/ProyectoWeb/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoWeb.Models;
+using System.Diagnostics;
+
+namespace ProyectoWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
